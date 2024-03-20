8e3f44e0b7b52d4f28d9c8397355cc4714f32a62
Jhay-Great <alemonai.jenef@gmail.com>
2024-03-20 14:17:52 +0000

    changed content-type in post request

--- frontend/src/helpers/httpRequest.js
+++ frontend/src/helpers/httpRequest.js
@@ -11,7 +11,8 @@ export const httpSubmitCollection = async function(data) {
     const response = await fetch(`${BASE_API}/collection`, {
         method: 'POST',
         headers: {
-            "Content-Type": "application/json",
+            "Content-Type": "multipart/form-data",
+            // "Content-Type": "application/json",
         },
         body: JSON.stringify(Object.fromEntries(data)),
     });
