5c202bdc74f0c0b2eff27d2aba3a0773fe3ce144
Jhay-Great <alemonai.jenef@gmail.com>
2024-04-20 14:07:48 +0000

    added remote server link

--- frontend/src/helpers/httpRequest.js
+++ frontend/src/helpers/httpRequest.js
@@ -1,4 +1,5 @@
-const BASE_API = 'http://localhost:8009'
+// const BASE_API = 'http://localhost:8009'
+const BASE_API = 'https://library-x1oq.onrender.com';
 
 // @Get request / getting entire collection
 export const httpGetEntireCollection = async function() {
